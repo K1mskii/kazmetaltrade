bdd56a3e955716da0e96afcc940099b0d156222f
K1mskii <movies2702@gmail.com>
2021-05-10 14:42:28 +0600

    add sections

--- js/main.js
+++ js/main.js
@@ -9,32 +9,18 @@ if(iconMenu) {
     })
 }
 
-// Прокрутка при клике
-const menuLinks = document.querySelectorAll('.menu__link[data-goto], .footer__link[data-goto]');
-if(menuLinks.length > 0) {
-    menuLinks.forEach(menuLink => {
-        menuLink.addEventListener("click", onMenuLinkClick);
-    });
 
-    function onMenuLinkClick(e) {
-        const menuLink = e.target;
-        if(menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
-            const gotoBlock = document.querySelector(menuLink.dataset.goto);
-            const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header, footer').offsetHeight;
-
-            // Закрытие меню при клике X
-            if(iconMenu.classList.contains('_active')) {
-                document.body.classList.remove('_lock');
-                iconMenu.classList.remove('_active');
-                menuBody.classList.remove('_active');
-            }
-
-            window.scrollTo({
-                top: gotoBlockValue,
-                behavior: "smooth"
-            });
-            e.preventDefault();
-        }
-    }
-}
-
+// Slider section
+document.addEventListener('DOMContentLoaded', () => {
+    AOS.init({
+        duration: 1200,
+    })
+    new fullpage('#fullpage', {
+        autoScrolling: true,
+        navigation: true,
+        navigationTooltips: ['Главная', 'О компании', 'Как заказать'],
+        showActiveTooltip: true,
+        dragAndMove: true,
+        anchors:['hero', 'about', 'order'],
+    })
+});

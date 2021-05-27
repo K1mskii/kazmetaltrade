c8454e274a285b7206598edb99dbe2e2757e7180
K1mskii <movies2702@gmail.com>
2021-05-27 16:31:50 +0600

    fix height fullpage

--- php/bd.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $db = new PDO('mysql:host=localhost;dbname=p-312670_kazmetal;charset=utf8', 'p-312_p-312670','4wQ8ju3?');
-        if(!isset($_SESSION)) 
-    { 
-        session_start(); 
-    } 
-?>
